afbcfc6abccad292116afe834e2b592d081ecbe3
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-05 04:45:34 +0530

    Improve UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import './App.css';
+
+import {
+  Box, Typography,
+} from '@mui/material';
+
+import CurrencyConverter from './Components/CurrencyConverter';
 
 function App() {
 
   return (
-    <div>
-      <h1>CurrencyXchange</h1>
-    </div>
+    <React.Fragment>
+      <Box sx={{ mt: 5, mb: 5, textAlign: "center" }}>
+        <Typography variant="h4">CurrencyXchange</Typography>
+        <Typography variant="subtitle1">Check live foreign currency exchange rates</Typography>
+      </Box>
+      <CurrencyConverter />
+    </React.Fragment>
   );
 }
 
